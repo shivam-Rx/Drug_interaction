2b32964ddde8c7f4c17946820f37e52ecb4a867a
shivam-rx <shivam.drx08@gmail.com>
2026-08-08 18:39:29 +0530

    test sync

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import pandas as pd
 # ----------------------------------------------------
 
 st.set_page_config(
-    page_title="AI Drug Interaction Prediction Tool",
+    page_title="DRUG SAFE-Drug Interaction Prediction Tool",
     page_icon="💊",
     layout="wide"
 )
